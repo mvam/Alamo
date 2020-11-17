c3ea8712607a4ff0d67c5ac6ebdcc5b41ad7bf11
Marcos Moreira <marcos12144@hotmail.com>
2020-11-17 11:01:25 -0300

    adição de interface abstractDAO

--- src/main/java/com/alamo/dao/AbstractJpaDAO.java
+++ src/main/java/com/alamo/dao/AbstractJpaDAO.java
@@ -13,36 +13,43 @@ import com.alamo.util.JpaUtil;
  * @param <T>
  * @param <I>
  */
-public abstract class AbstractJpaDAO<T,I> {
+public abstract class AbstractJpaDAO<T,I> implements IAbstractJpaDAO<T, I> {
 	
+	@Override
 	public EntityManager getEntityManager() {
 		return JpaUtil.getEntityManager();
 	}
 	
 
+	@Override
 	public T update(T object){
 		this.getEntityManager().merge(object);
 		return object;
 	}
 	
+	@Override
 	public void delete(T object){		
 		this.getEntityManager().remove(object);
 	}
 	
+	@Override
 	public void delete(Class<T> type,I id){		
 		T object = this.getById(type, id);
 		this.getEntityManager().remove(object);
 	}
 	
+	@Override
 	public T create(T object){
 		this.getEntityManager().persist(object);
 		return object;
 	}
 	
+	@Override
 	public T getById(Class<T> type,I id){
 		return (T)this.getEntityManager().find(type, id);
 	}
 
+	@Override
 	@SuppressWarnings("unchecked")
 	public List<T> find(String jpql){
 		 Query query = getEntityManager().createQuery(jpql);
